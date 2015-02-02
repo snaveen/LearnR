730180b5a7bfddff1c4b84fd4a5b6dfc1f05a215
milli <gowrishankar.milli@gmail.com>
2015-02-01 21:13:10 -0800

    added missing dependencies

--- examples/camel-ex/src/test/java/com/examples/camel/RoutingXmlTest.java
+++ examples/camel-ex/src/test/java/com/examples/camel/RoutingXmlTest.java
@@ -20,7 +20,7 @@ public class RoutingXmlTest {
 	@SuppressWarnings("deprecation")
 	public void run() throws Exception{
 		CamelContext context = new DefaultCamelContext();
-		InputStream is = getClass().getResourceAsStream("test.xml");
+		InputStream is = getClass().getResourceAsStream("routing.xml");
 		RoutesDefinition routes = context.loadRoutesDefinition(is);
 		context.addRouteDefinitions(routes.getRoutes());
 		context.start();
