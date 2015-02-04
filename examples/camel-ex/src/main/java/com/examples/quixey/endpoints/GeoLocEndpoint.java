76ac7de6ca2d3cf17d562c7810a0dc7ffafe5160
Manoj <manojiitkumar93@gmail.com>
2015-02-04 19:16:16 +0530

    added code for routing geolocation endpoints and trnsformation of the response.

--- examples/camel-ex/src/main/java/com/examples/quixey/endpoints/GeoLocEndpoint.java
+++ examples/camel-ex/src/main/java/com/examples/quixey/endpoints/GeoLocEndpoint.java
@@ -1,13 +1,57 @@
 package com.examples.quixey.endpoints;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GeoLocEndpoint extends RouteBuilder {
-
 	@Override
 	public void configure() throws Exception {
-		// TODO Auto-generated method stub
-		
+		from("jetty:http://localhost:5000/sdk/android/v1/geolocate")
+				.process(new Processor() {
+
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						// TODO Auto-generated method stub
+						System.out.println("@@@@@ :"
+								+ exchange.getIn().getHeaders());
+						exchange.getIn().setHeader(
+								Exchange.HTTP_QUERY,
+								"ip_address="
+										+ exchange.getIn().getHeader(
+												"client_ip"));
+
+					}
+				})
+				.to("http://geo-service-i-5a91fe99.us-west-1c.stage.quixey.com:10106/geodata?bridgeEndpoint=true&throwExceptionOnFailure=false")
+				.process(new Processor() {
+
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						// TODO Auto-generated method stub
+						String inBody = exchange.getIn().getBody(String.class);
+						System.out.println(inBody);
+						String outBody = transformation(inBody);
+						exchange.getOut().setBody(outBody);
+					}
+
+					private String transformation(String inBody)
+							throws JSONException {
+						// TODO Auto-generated method stub
+						JSONObject ob = new JSONObject(inBody);
+						System.out.println(ob.get("edge") + "****************");
+						String country = new JSONObject(ob.getString("edge"))
+								.getString("country");
+						String cou = "Country :".concat(country);
+						String city = new JSONObject(ob.getString("edge"))
+								.getString("city");
+						String c = "City :".concat(city);
+						String result = "{" + cou + "," + c + "}";
+						return result;
+					}
+				});
+
 	}
-	
 }
--- examples/camel-ex/src/test/java/com/examples/camel/apibox/ContentBased.java
+++ examples/camel-ex/src/test/java/com/examples/camel/apibox/ContentBased.java
@@ -1,15 +1,29 @@
 package com.examples.camel.apibox;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 import com.examples.camel.apibox.util.ToValidJson;
 
 public class ContentBased extends RouteBuilder {
+	public static Object value;
 	
 	@Override
 	public void configure() throws Exception {
 
-		from("jetty:http://0.0.0.0:8080/content")
+		from("jetty:http://0.0.0.0:8080/content").process(new Processor(){
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				// TODO Auto-generated method stub
+				System.out.println("THis is what we want: "+exchange.getIn().getHeader("service"));
+				System.out.println("This are headers :"+exchange.getIn().getHeaders());
+				System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+				value = exchange.getIn().getHeader("service");
+			}
+			
+		})
 		.choice()
 			.when(header("service").isEqualTo("customer"))
 				.to("http://localhost:5000/customer?bridgeEndpoint=true&throwExceptionOnFailure=false").process(new ToValidJson())
