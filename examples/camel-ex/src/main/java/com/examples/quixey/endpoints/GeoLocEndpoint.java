1d62564fa56c0223c2618b6a8d1e5c793f7abb82
milli <gowrishankar.milli@gmail.com>
2015-02-04 13:40:20 -0800

    examples : camel : more cleanup and code refactoring

--- examples/camel-ex/src/main/java/com/examples/quixey/endpoints/GeoLocEndpoint.java
+++ examples/camel-ex/src/main/java/com/examples/quixey/endpoints/GeoLocEndpoint.java
@@ -3,55 +3,49 @@ package com.examples.quixey.endpoints;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class GeoLocEndpoint extends RouteBuilder {
+
+	public static final String ENDPOINT_URL = "http://localhost:8080/sdk/android/v1/geolocate";
+	public static final String MTS_URL = "http://geo-service-i-5a91fe99.us-west-1c.stage.quixey.com:10106/geodata";
+
 	@Override
 	public void configure() throws Exception {
-		from("jetty:http://localhost:5000/sdk/android/v1/geolocate")
-				.process(new Processor() {
 
-					@Override
-					public void process(Exchange exchange) throws Exception {
-						// TODO Auto-generated method stub
-						System.out.println("@@@@@ :"
-								+ exchange.getIn().getHeaders());
-						exchange.getIn().setHeader(
-								Exchange.HTTP_QUERY,
-								"ip_address="
-										+ exchange.getIn().getHeader(
-												"client_ip"));
-
-					}
-				})
-				.to("http://geo-service-i-5a91fe99.us-west-1c.stage.quixey.com:10106/geodata?bridgeEndpoint=true&throwExceptionOnFailure=false")
-				.process(new Processor() {
-
-					@Override
-					public void process(Exchange exchange) throws Exception {
-						// TODO Auto-generated method stub
-						String inBody = exchange.getIn().getBody(String.class);
-						System.out.println(inBody);
-						String outBody = transformation(inBody);
-						exchange.getOut().setBody(outBody);
-					}
-
-					private String transformation(String inBody)
-							throws JSONException {
-						// TODO Auto-generated method stub
-						JSONObject ob = new JSONObject(inBody);
-						System.out.println(ob.get("edge") + "****************");
-						String country = new JSONObject(ob.getString("edge"))
-								.getString("country");
-						String cou = "Country :".concat(country);
-						String city = new JSONObject(ob.getString("edge"))
-								.getString("city");
-						String c = "City :".concat(city);
-						String result = "{" + cou + "," + c + "}";
-						return result;
-					}
-				});
+		from("jetty:" + ENDPOINT_URL)
+		.process(new GeoEndpointProcessor())
+			.to(MTS_URL + "?bridgeEndpoint=true&throwExceptionOnFailure=false")
+			.process(new GeoJsonProcessor());
 
 	}
+
+	/* --- Utilities --- */
+
+	class GeoEndpointProcessor implements Processor {
+
+		@Override
+		public void process(Exchange exchange) throws Exception {
+			Object client_ip = exchange.getIn().getHeader("client_ip");
+			exchange.getIn().setHeader(Exchange.HTTP_QUERY, "ip_address=" + client_ip);
+		}
+
+	}
+
+	class GeoJsonProcessor implements Processor {
+
+		@Override
+		public void process(Exchange exchange) throws Exception {
+			JSONObject in = new JSONObject(exchange.getIn().getBody(String.class));
+
+			// prepare out
+			JSONObject out = new JSONObject();
+			out.put("country", in.getJSONObject("edge").getString("country"));
+			out.put("city", in.getJSONObject("edge").getString("city"));
+
+			exchange.getOut().setBody(out);
+		}
+	
+	}
+	
 }
--- examples/camel-ex/src/test/java/com/examples/camel/apibox/APIBoxAggregator.java
+++ examples/camel-ex/src/test/java/com/examples/camel/apibox/APIBoxAggregator.java
@@ -1,28 +1,51 @@
 package com.examples.camel.apibox;
 
+import java.util.List;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import com.examples.camel.apibox.util.SimpleAggrStrategy;
-
+/**
+ * Reference: http://stackoverflow.com/questions/10610820/apache-camel-to-aggregate-multiple-rest-service-responses
+ */
 public class APIBoxAggregator extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		
-		// http://stackoverflow.com/questions/10610820/apache-camel-to-aggregate-multiple-rest-service-responses
-		
-		
-		from("jetty:http://0.0.0.0:8080/aggr")
-			.enrich("direct:service_aggregate").setBody(property(Exchange.GROUPED_EXCHANGE));
-	
-		from("direct:service_aggregate")
-		  .multicast(new SimpleAggrStrategy()).parallelProcessing()
-		    .enrich("http://localhost:5000/customer?bridgeEndpoint=true&throwExceptionOnFailure=false")
-		    .enrich("http://localhost:5000/order?bridgeEndpoint=true&throwExceptionOnFailure=false")
-		  .end();
-		
-		
+
+		from("jetty:http://0.0.0.0:8080/aggr").enrich("direct:service_aggregate").setBody(
+				property(Exchange.GROUPED_EXCHANGE));
+
+		from("direct:service_aggregate").multicast(new SimpleAggrStrategy()).parallelProcessing()
+				.enrich("http://localhost:5000/customer?bridgeEndpoint=true&throwExceptionOnFailure=false")
+				.enrich("http://localhost:5000/order?bridgeEndpoint=true&throwExceptionOnFailure=false").end();
+
 	}
-	
+
+	/* --- Utilities --- */
+
+	class SimpleAggrStrategy extends GroupedExchangeAggregationStrategy {
+
+		@Override
+		public void onCompletion(Exchange exchange) {
+
+			List<Exchange> list = (List<Exchange>) exchange.getProperty(Exchange.GROUPED_EXCHANGE);
+			try {
+				JSONObject out = new JSONObject();
+				for (int i = 0; i < list.size(); i++) {
+					String inStr = list.get(i).getIn().getBody(String.class);
+					Object outStr = inStr.startsWith("[") ? new JSONArray(inStr) : new JSONObject(inStr);
+					out.put("result" + i, outStr);
+				}
+				exchange.setProperty(Exchange.GROUPED_EXCHANGE, out);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 }
